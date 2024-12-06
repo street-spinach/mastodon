d412049878820b35cd4c0f1e59bcabd547437098
madushankaf <madushankafonseka@live.com>
2024-12-06 08:13:59 +0530

    corrected cert path names

--- config/puma.rb
+++ config/puma.rb
@@ -14,9 +14,9 @@ end
 
 # SSL Bind for HTTPS
 ssl_bind '0.0.0.0', '3000', {
-  key: '/etc/ssl/private/key.pem',  # Path to your private key
-  cert: '/etc/ssl/private/cert.pem' # Path to your certificate
-} if ENV['ENABLE_SSL'] == 'true'
+  key: '/etc/ssl/private/selfsigned.key',  # Path to your private key
+  cert: '/etc/ssl/private/selfsigned.crt' # Path to your certificate
+} 
 
 environment ENV.fetch('RAILS_ENV') { 'development' }
 workers     ENV.fetch('WEB_CONCURRENCY') { 2 }.to_i
