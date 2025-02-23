dc9a3837dd89e3e2f950a082369698d876b9d118
madushankaf <madushankafonseka@live.com>
2025-02-23 22:52:48 +0530

    chaged port binding in side car app

--- sidecar/index.js
+++ sidecar/index.js
@@ -56,6 +56,6 @@ app.post('/sidecar_proxy/auth_exchange', (req, res) => {
  * 
  * @returns {void}
  */
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
